2ec023589190b3788ef6216613d5ab203ab351f5
vyogesh583 <67259533+vyogesh583@users.noreply.github.com>
2020-06-25 13:51:46 +0530

    initial

---
+++ app/src/main/java/com/yvtechnologies/exoplayerinjava/MainActivity.java
@@ -0,0 +1,63 @@
+package com.yvtechnologies.exoplayerinjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private VideoPlayerRecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+
+       LinearLayoutManager layoutManager;
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+
+        ArrayList<MediaObject> mediaObjects = new ArrayList<MediaObject>(Arrays.asList(Resources.MEDIA_OBJECTS));
+        mRecyclerView.setMediaObjects(mediaObjects);
+        VideoPlayerRecyclerAdapter adapter = new VideoPlayerRecyclerAdapter(mediaObjects, initGlide());
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private RequestManager initGlide(){
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
+
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if(mRecyclerView!=null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/yvtechnologies/exoplayerinjava/Resources.java
@@ -0,0 +1,28 @@
+package com.yvtechnologies.exoplayerinjava;
+
+public class Resources {
+
+    public static final MediaObject[] MEDIA_OBJECTS = {
+
+            new MediaObject("image",
+                    "",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/104457577_727541771342828_7992101294237688127_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nZjasZzK2t8AX-TG_QN&oh=1730f1bc110d116abe68dc2b2d62ef80&oe=5F1B0BD7",
+                    "Image Post"),
+            new MediaObject("video",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t50.2886-16/104553655_188536192585680_6482485010549440459_n.mp4?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=Jrkn0onKm4QAX8y6fKt&oe=5EF43F4E&oh=9ff33669f097a4f232c89ba3ff724d72",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/104457577_727541771342828_7992101294237688127_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nZjasZzK2t8AX-TG_QN&oh=1730f1bc110d116abe68dc2b2d62ef80&oe=5F1B0BD7",
+                    "Video Post"),
+            new MediaObject("video",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t50.2886-16/104500405_664626457456453_1493085377276099694_n.mp4?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=QBdrBYlZHsAAX-TvlZP&oe=5EF45B69&oh=8c4981bb6be701ed73d7fa758780f97c",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/104457577_727541771342828_7992101294237688127_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nZjasZzK2t8AX-TG_QN&oh=1730f1bc110d116abe68dc2b2d62ef80&oe=5F1B0BD7",
+                    "Video Post"),
+            new MediaObject("image",
+                    "",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/104457577_727541771342828_7992101294237688127_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nZjasZzK2t8AX-TG_QN&oh=1730f1bc110d116abe68dc2b2d62ef80&oe=5F1B0BD7",
+                    "Image Post"),
+            new MediaObject("video",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t50.2886-16/104321490_630685937794995_6874406234758481542_n.mp4?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-RYlMQ8DfdUAX-bIOpl&oe=5EF3E031&oh=cafff9d9faa0a9cff08787e10e2d46aa",
+                    "https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/104457577_727541771342828_7992101294237688127_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=nZjasZzK2t8AX-TG_QN&oh=1730f1bc110d116abe68dc2b2d62ef80&oe=5F1B0BD7",
+                    "Video Post"),
+    };
+}
